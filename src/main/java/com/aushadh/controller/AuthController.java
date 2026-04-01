c41546dfb89e92a085c99e38ad093ddb931a0858
Pachamatla Charan Sai Venkata Varma <163965348+CharanVarma27@users.noreply.github.com>
2026-04-01 10:01:13 +0530

    Fix CrossOrigin annotation formatting

--- src/main/java/com/aushadh/controller/AuthController.java
+++ src/main/java/com/aushadh/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin( origin="https://aushadh.onrender.com/")
+@CrossOrigin(origins = "https://aushadh.onrender.com/")
 public class AuthController {
 
     @Autowired
