1455d1ecb0a0ef017aab7d8bb1524c77dd470b74
Pachamatla Charan Sai Venkata Varma <163965348+CharanVarma27@users.noreply.github.com>
2026-04-01 10:01:35 +0530

    Update CrossOrigin annotation in MedicineController

--- src/main/java/com/aushadh/controller/MedicineController.java
+++ src/main/java/com/aushadh/controller/MedicineController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/medicines")
-@CrossOrigin
+@CrossOrigin(origins = "https://aushadh.onrender.com/")
 public class MedicineController {
 
     @Autowired
